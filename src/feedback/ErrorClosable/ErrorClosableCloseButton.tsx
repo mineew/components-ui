d0f532a3130f7b5107ba48ee33a4bae258e0acd3
Mineev Alexander <mineew.al@gmail.com>
2023-05-12 20:18:37 +0300

    ErrorClosable: added tooltip to close button

--- src/feedback/ErrorClosable/ErrorClosableCloseButton.tsx
+++ src/feedback/ErrorClosable/ErrorClosableCloseButton.tsx
@@ -1,6 +1,8 @@
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 
+import Tooltip from '../Tooltip/Tooltip';
+
 interface ErrorClosableCloseButtonProps {
   onClick: () => void;
 }
@@ -8,20 +10,28 @@ interface ErrorClosableCloseButtonProps {
 function ErrorClosableCloseButton(props: ErrorClosableCloseButtonProps) {
   const { onClick } = props;
 
-  const classes = [];
+  const buttonClasses = [];
+  const positionClasses = [];
 
-  classes.push('block', 'w-5', 'h-5', 'rounded-md');
-  classes.push('absolute', 'top-2', 'right-3');
-  classes.push('hover:bg-white/40');
-  classes.push('focus:bg-white/40', 'focus:outline');
-  classes.push('outline-2', 'outline-offset-2', 'outline-white');
-  classes.push('active:bg-white', 'active:text-red-500');
-  classes.push('transition');
+  buttonClasses.push('block', 'w-5', 'h-5', 'rounded-md');
+  buttonClasses.push('hover:bg-white/40');
+  buttonClasses.push('focus:bg-white/40', 'focus:outline');
+  buttonClasses.push('outline-2', 'outline-offset-2', 'outline-white');
+  buttonClasses.push('active:bg-white', 'active:text-red-500');
+  buttonClasses.push('transition');
+
+  positionClasses.push('absolute', 'top-2', 'right-2');
 
   return (
-    <button className={classNames(classes)} type="button" onClick={onClick}>
-      <XMarkIcon className={classNames('w-5', 'h-5')} strokeWidth={2} />
-    </button>
+    <Tooltip className={classNames(positionClasses)} title="Закрыть">
+      <button
+        className={classNames(buttonClasses)}
+        type="button"
+        onClick={onClick}
+      >
+        <XMarkIcon className={classNames('w-5', 'h-5')} strokeWidth={2} />
+      </button>
+    </Tooltip>
   );
 }
 
