12e163d7ceeaeb95de12374dbe95c8b85e34345e
Mineev Alexander <mineew.al@gmail.com>
2023-06-04 15:03:30 +0300

    added exports from inputs

--- src/inputs/index.ts
+++ src/inputs/index.ts
@@ -2,6 +2,17 @@ import Button from './Button';
 import Checkbox from './Checkbox';
 import Input from './Input';
 import PasswordInput from './PasswordInput';
+import { Radio, RadioGroup } from './Radio';
+import Select from './Select';
 import Switch from './Switch';
 
-export { Button, Checkbox, Input, PasswordInput, Switch };
+export {
+  Button,
+  Checkbox,
+  Input,
+  PasswordInput,
+  Radio,
+  RadioGroup,
+  Select,
+  Switch,
+};
